0ffb82bd5851c16ee0987c79fa816dad2b3e6e73
auralshin <anshulspartan141@gmail.com>
2020-11-26 04:10:37 +0530

    auth added,   fix the error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
 import './App.css';
+import { DID } from "dids";
+import CeramicClient from "@ceramicnetwork/ceramic-http-client";
+import { IDXWeb } from "@ceramicstudio/idx-web";
+import { publishIDXConfig } from "@ceramicstudio/idx-tools";
+import {
+  ThreeIdConnect,
+  EthereumAuthProvider,
+} from "../../node_modules/3id-connect/src/index";
+
+import { Button } from 'antd';
+
+var idx =  {}
+var ceramicb= {}
+var did = {}
+var profile = {}
+var id =  ""
+var ethaddress = ""
+var auth=  false
+
+async function saveProfile() {
+  const profileData = profiles.map(profile => ({
+    y: table.y,
+    profile:profile.profile,
+    did:profile.id,
+    ethadress:profile.ethaddress,
+    method:profile.update,
+  }),
+
+  dispatch(Actions.save(profiles.channel, profileData)),
+}
+
+async function Authenticate () {
+  const THREEID_CONNECT_URL = "https://3idconnect.org/index.html";
+  const DEFAULT_API_URL = "https://ceramic.3boxlabs.com";
+  const API_URL = "http://localhost:7007";
+  const threeIdConnect = new ThreeIdConnect(THREEID_CONNECT_URL);
+  const ceramic = new CeramicClient(DEFAULT_API_URL);
+  //auth
+  const ethProvider = await web3Modal.connect();
+  const addresses = await ethProvider.request({ method: "eth_accounts" });
+  console.log("Got the ethaddress");
+  this.ethaddress = addresses[0];
+  const authProvider = new EthereumAuthProvider(ethProvider, addresses[0]);
+  await threeIdConnect.connect(authProvider);
+  console.log("3id connect func executed");
+  const didProvider = await threeIdConnect.getDidProvider();
+  console.log("didProvider accessed");
+  //console.dir( didProvider)
+  const did = new DID({ provider: didProvider });
+  //console.dir(did)
+  await did.authenticate();
+  console.log("This is the did " + did.id);
+  const jws = await did.createJWS({ hello: "world" });
+  console.log(jws);
+  const { definitions } = await publishIDXConfig(ceramic);
+  console.log("The Definitions");
+  console.log(definitions);
+  const appDefinitions = {
+    profile: definitions.basicProfile,
+  };
+  await ceramic.setDIDProvider(didProvider);
+  console.log("Ceramic DID Provider set");
+  const idx = new IDXWeb({
+    ceramic,
+    definitions: appDefinitions,
+    connect: threeIdConnect,
+  });
+  console.log("new idx instance created");
+  const ethereum = { provider: ethProvider, address: addresses[0] };
+  await idx.authenticate(ethereum);
+  if (idx.authenticated) {
+    console.log("authenticated IDX!!");
+    //redirect upon this
+    this.auth = true;
+    this.id = idx.id;
+    if (this.auth) {
+      const MyProfile = await idx.get("profile", idx.id);
+      this.profile = MyProfile;
+      console.log("My Profile");
+      console.dir(MyProfile);
+    }
+    this.idx = idx;
+    this.ceramic = ceramic;
+    this.did = did;
+    this.component = "card3";
+  }
+}
+async function Edit() {
+  if (this.auth) {
+    this.component = "card4";
+  }
+}
+async function Update (profile2) {
+  if (this.idx.authenticated) {
+    console.log("update");
+    const doc = await this.idx.set("profile", profile2);
+    console.log(doc);
+    const MyProfile = await this.idx.get("profile", this.idx.id);
+    this.profile = MyProfile;
+    this.component = "card3";
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button type="primary" style={{padding:0 , position:"auto"}} onClick={ Authenticate(),
+            Edit(),
+            Update(),
+            saveProfile()
+            } >
+      Primary Button
+    </Button>
     </div>
   );
 }
